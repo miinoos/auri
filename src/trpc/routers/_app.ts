2c286e2fb21aeb730cb8b639870fade3d92e5781
Yudhajit Chowdhury <yudhajitchowdhury17@gmail.com>
2025-08-25 14:53:34 +0530

    initializing inngest for background jobs

--- src/trpc/routers/_app.ts
+++ src/trpc/routers/_app.ts
@@ -1,16 +1,20 @@
 import { z } from "zod";
 import { baseProcedure, createTRPCRouter } from "../init";
+import { inngest } from "@/inngest/client";
 export const appRouter = createTRPCRouter({
-  hello: baseProcedure
+  invoke: baseProcedure
     .input(
       z.object({
         text: z.string(),
       })
     )
-    .query((opts) => {
-      return {
-        greeting: `hello ${opts.input.text}`,
-      };
+    .mutation(async ({ input }) => {
+      await inngest.send({
+        name: "test/hello.world",
+        data: {
+          email: input.text,
+        },
+      });
     }),
 });
 // export type definition of API
